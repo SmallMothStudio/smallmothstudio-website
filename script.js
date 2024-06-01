13193aa8ed6089d23aeb8214e7c62ab06fb67493
Vincent Myers <145707270+SmallMothStudio@users.noreply.github.com>
2024-05-31 23:32:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,3 @@ new Twitch.Embed("twitch-embed", {
     channel: twitchChannel,
     layout: "video-with-chat",
 });
-
-window.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            section.classList.add('visible');
-        }
-    });
-});
